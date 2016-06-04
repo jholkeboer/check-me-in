6b1e71d1b6b6c2d52b368a0c282bf93164838051
jholkeboer <jholkeboer@users.noreply.github.com>
2016-06-04 13:21:54 -0700

    login

--- models.py
+++ models.py
@@ -2,17 +2,6 @@ from google.appengine.ext import db
 import json
 
 
-# class Recipe(db.Model):
-#     name = db.StringProperty(required=True)
-#     prepTime = db.IntegerProperty(required=True)
-#     category = db.TextProperty(required=True)
-#     ingredients = db.ListProperty(db.Key, required=True)
-#     instructions = db.StringListProperty(required=True)
-
-# class Ingredient(db.Model):
-#     name = db.StringProperty(required=True)
-#     usedIn = db.ListProperty(db.Key)
-
 class CheckIn(db.Model):
     owner = db.UserProperty(required=True)
     description = db.TextProperty()
